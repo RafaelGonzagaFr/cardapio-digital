588422865020e1f55296db3c59853f3b7db996bb
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2024-01-22 21:57:24 -0300

    Feat: Implements Post Method

--- src/main/java/com/example/cardapio/CardapioApplication.java
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CardapioApplication {
+public class   CardapioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardapioApplication.class, args);
