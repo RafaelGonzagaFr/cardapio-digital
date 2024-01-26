6b721fa527522d404c13599547d002f9af4ef5c0
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2024-01-25 21:14:48 -0300

    Feat: Implements FoodData POST

---
+++ src/main/java/com/example/cardapio/view/cardapio/src/components/create-modal/create-modal.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import { useFoodDataMutate } from "../../hooks/useFoodDataMutate";
+import { FoodData } from "../../interfaces/FoodData";
+import "./modal.css";
+
+interface InputProps {
+    label: string,
+    value: string | number,
+    updateValue(value: any): void
+}
+
+interface ModalProps {
+    closeModal(): void;
+}
+
+const Input = ({ label, value, updateValue}: InputProps) => {
+    return (
+        <>
+            <label>{label}</label>
+            <input value={value} onChange={e => updateValue(e.target.value)}></input>
+        </>
+    )
+}
+
+export function CreateModal( {closeModal}: ModalProps ){
+
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [image, setImage] = useState("");
+    const { mutate, isSuccess, isLoading } = useFoodDataMutate();
+
+    const submit = () => {
+        const foodData : FoodData = {
+            title,
+            price,
+            image
+        }
+
+        mutate(foodData)
+    }
+
+    useEffect(() => {
+        if(!isSuccess) return
+        closeModal();
+    }, [isSuccess])
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-body">
+                <h2>Cadastre um novo item no cardápio</h2>
+                <form action="" className="input-container">
+                    <Input label="Title" value={title} updateValue={setTitle}/>
+                    <Input label="Price" value={price} updateValue={setPrice}/>
+                    <Input label="Image" value={image} updateValue={setImage}/>
+
+                </form>
+                <button onClick={submit} className="btn-secondary">  {isLoading ? 'postando...' : 'postar'} </button>
+            </div>
+
+        </div>
+    )
+
+}
